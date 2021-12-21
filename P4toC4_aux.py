ccb221a85b8bb29455bb16d51d6ed26a33142be2
Thomas Sommerfeld <tsmmrfld@gmail.com>
2021-12-20 20:18:01 -0600

    basis_mapping() works for p-functions; needs polishing and d-function map

--- P4toC4_aux.py
+++ P4toC4_aux.py
@@ -42,8 +42,111 @@ def psi4_to_c4(Cp4, offset):
     return Cc4
 
 
+def basis_mapping(basisset, verbose=1):
+    """
+    Cfour4_MO[i] = Psi4_MO[map[i]]
+    
+    for Cfour, we assume all s, then all p, ...
+    
+    therefore in a Cfour_MO vector: s, s, s, px, px, py, py, pz, pz, d ...
+    for every atom
+    
+    so for every atom we need first the offsets: shell_offset[l,m]
+    then we can go over the atom-basis again and compute map[i]
+    
+    Parameters
+    ----------
+    basisset : psi4.core.basisset, Psi4 class for basis sets
+    verbose : int, output level.
+
+    Returns
+    -------
+    map : int np.array to be used in psi4_to_c4()
+
+    """
+    max_l = 5
+    max_m = 2*max_l + 1
+    
+    mol = basisset.molecule()
+    n_atoms = mol.natom()
+    nbf_atom = np.zeros(n_atoms, int)
+    nbf = basisset.nbf()
+    map = np.zeros(nbf, int)
+
+    """ map from Psi4 to Cfour m-order 
+        (z,x,y) -> (x,y,z)
+    
+    
+    """
+    m_map = np.array(
+        [[0, 0, 0, 0, 0],
+         [2, 1, 0, 0, 0],
+         [0, 1, 2, 3, 4],
+         [0, 1, 2, 3, 4]]
+        )                   
+
+
+    atom_offset = 0  # identical for Psi4_MO and Cfour_MO
+    i_mo = 0
+
+    for j_atom in range(mol.natom()):
+        if verbose > 0:
+            print(f'Atom {j_atom}')
+        """ counter for shells for each angular momentum """
+        nl = np.zeros(max_l, int)  
+        nm = 2*np.arange(max_l)+1  # assume all shells are spherical
+
+        """ initial sweep: establish shell_offset[l,m] """
+        for i_shell in range(basisset.nshell_on_center(j_atom)):
+            shell = basisset.shell(j_atom, i_shell)
+            am = shell.amchar
+            l = shell.am
+            if verbose > 0:
+                print(f"  Shell {i_shell}  {am} {l}")
+            nl[l] += 1
+        nbf_atom[j_atom] = np.sum(nl*nm)
+        
+        """ offset in the Cfour_MO vector: shell_offset[l,m] IMHO """
+        shell_offset = np.zeros((6,9), int)
+        rel_offset = 0
+        shell_offset[0,0] = 0
+        for l in range(1, max_l):
+            rel_offset += nl[l-1]
+            shell_offset[l,0] = rel_offset
+            for m in range(1, 2*l+1):
+                rel_offset += nl[l]
+                shell_offset[l,m] = rel_offset
+        if verbose > 1:
+            print('shell_offset[l,m]:')
+            print(shell_offset)
+
+        """ second sweep: """
+        nl = np.zeros(max_l, int)
+        for i_shell in range(basisset.nshell_on_center(j_atom)):
+            shell = basisset.shell(j_atom, i_shell)
+            l = shell.am
+            print(f'l = {l}, nl[l]={nl[l]}')
+            for m in range(2*l+1):
+                m_cfour = m_map[l,m]
+                ptr = atom_offset + shell_offset[l,m_cfour] + nl[l]
+                print(f'  m={m} -> m={m_cfour}   ptr={ptr}')
+                map[i_mo] = ptr
+                i_mo += 1
+            nl[l] += 1
+        
+        atom_offset += nbf_atom[j_atom]
+
+
+    if verbose > 0:    
+        print(f'nbf = {nbf}   sum(nbf_atom)={np.sum(nbf_atom)}')
+    
+    return map
+
+
 def ao_offset(basisset, verbose=0):
     """
+    nice, but works only for STO-3G
+    
     offset vector for Psi4 to Cfour MO mapping
     
     Cfour_MO[i+offset[i]] = Psi4_MO[i]
